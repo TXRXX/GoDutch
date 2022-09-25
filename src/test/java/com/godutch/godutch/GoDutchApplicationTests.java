02a7619b04f1c69b28d68bec89b572a56ce525d7
TXRXX <toro2544@gmail.com>
2022-09-25 16:58:17 +0700

    Init spring project and connect to mongodb atlast

---
+++ src/test/java/com/godutch/godutch/GoDutchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godutch.godutch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoDutchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
