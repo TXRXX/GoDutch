f68df56dc91bf4f7b6bad32e0cd7275b18f55510
wave010 <thirawat.p@kkumail.com>
2022-09-30 19:33:20 +0700

    aaa

--- src/main/java/com/godutch/godutch/Controller/DutchController.java
+++ src/main/java/com/godutch/godutch/Controller/DutchController.java
@@ -1,6 +1,7 @@
 package com.godutch.godutch.Controller;
 
 import com.godutch.godutch.Repository.DutchRepository;
+import com.godutch.godutch.Repository.ItemRepository;
 import com.godutch.godutch.Service.SequenceGeneratorService;
 import com.godutch.godutch.model.Dutch;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,21 @@ import org.springframework.web.bind.annotation.*;
 public class DutchController {
     @Autowired
     private DutchRepository DutchRepo;
+
+    @Autowired
+    private ItemRepository ItemRepo;
     @Autowired
     private SequenceGeneratorService service;
 
 
 //    method save data to database (create)
-    @PostMapping("/create-dutch")
+    @GetMapping("/add-dutch")
     public String addDutch(@Validated Dutch dutch, BindingResult result, Model model){
         if(result.hasErrors()){
             return "create-dutch";
         }
         dutch.setId(service.getSequenceNumber(Dutch.SEQUENCE_NAME));
+        dutch.setItemList(ItemRepo.findAll());
         DutchRepo.save(dutch);
         System.out.println("Add dutch");
         return "redirect:/";
