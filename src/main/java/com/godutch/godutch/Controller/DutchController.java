b4f5d7d07768c0a2db1238701f92035cfc97b859
wave010 <thirawat.p@kkumail.com>
2022-09-30 21:31:50 +0700

    edit-dutch

--- src/main/java/com/godutch/godutch/Controller/DutchController.java
+++ src/main/java/com/godutch/godutch/Controller/DutchController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Controller
 public class DutchController {
     @Autowired
@@ -23,7 +25,7 @@ public class DutchController {
     private SequenceGeneratorService service;
 
 
-//    method save data to database (create)
+//    method save data to database and Delete all Item in database
     @PostMapping("/add-dutch")
     public String addDutch(@Validated Dutch dutch, BindingResult result, Model model){
         if(result.hasErrors()){
@@ -33,6 +35,16 @@ public class DutchController {
         dutch.setItemList(ItemRepo.findAll());
         DutchRepo.save(dutch);
         System.out.println("Add dutch");
+        ItemRepo.deleteAll();
         return "redirect:/";
+    }
+
+    //function to show EditDutch by id
+    @GetMapping("/edit-dutch/{id}")
+    public String editDutch(@PathVariable("id") int id,Model model){
+        Dutch dutch = DutchRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid dutch id: "+ id));
+        model.addAttribute("dutch",dutch);
+        return "edit-dutch";
     }
 }
