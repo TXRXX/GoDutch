1e09a611d1daf0bfa0f591bf378f9a4140e9e2fb
TXRXX <toro2544@gmail.com>
2022-10-05 14:47:14 +0700

    prevent string and negative value

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,8 @@
+
+setTimeout(() => {
+    const box = document.getElementById('loading');
+    box.style.display = 'none';
+}, 1600); // 👈️ time in milliseconds
+
+const priceInput = document.getElementById('create__price');
+
